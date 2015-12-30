ec193f2554a392c99d8085e2e4abd0198a63d3aa
slavidlancer <slavidlancer@users.noreply.github.com>
2015-12-31 00:08:46 +0200

    Added modified, additional and latest sources to ConsoleGame in Java

--- Java/training_from_tutorials/games/ConsoleGameExample/src/com/company/games/Game.java
+++ Java/training_from_tutorials/games/ConsoleGameExample/src/com/company/games/Game.java
@@ -2,8 +2,10 @@ package com.company.games;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import com.company.games.elements.Element;
 import com.company.games.elements.Player;
+import com.company.games.maps.MapReader;
 
 public class Game {
 	private Player playerOne;
@@ -56,6 +58,15 @@ public class Game {
 	public void setTurnChanged(boolean turnChanged) {
 		this.turnChanged = turnChanged;
 	}
+	
+	public void switchTurn() {
+        if (this.currentPlayer == this.getPlayerOne()) {
+            this.currentPlayer = this.getPlayerTwo();
+        } else {
+            this.currentPlayer = this.getPlayerOne();
+        }
+
+    }
 
 	public void setRunning(boolean running) {
 		this.running = running;
@@ -65,14 +76,98 @@ public class Game {
 		return this.running;
 	}
 	
+	public void setOutput(String output) {
+        this.output += "\n" + output;
+    }
+	
 	private void printOutput() {
         System.out.println(this.output);
         
         this.output = "";
     }
+	
+	private void start() {
+		Scanner input = new Scanner(System.in);
+		
+		while (this.isRunning()) {
+			if (this.isTurnChanged()) {
+                System.out.println(this.currentPlayer.getType() +
+                		" is on turn");
+                
+                this.setTurnChanged(false);
+            }
+			
+			String cmd = input.nextLine();
+			
+			switch (cmd) {
+			case "map":
+				for (int i = 0; i < this.map.size(); i++) {
+                    for (int j = 0; j < this.map.get(i).size(); j++) {
+                        System.out.printf("%s : %s -> ", i, j);
+                        System.out.println(this.map.get(i).get(j).getType());
+                    }
+                }
+				
+				break;
+				
+			case "move up":
+				try {
+                    this.currentPlayer.moveUp();
+                    this.printOutput();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+				
+				break;
+				
+			case "move right":
+				try {
+                    this.currentPlayer.moveRight();
+                    this.printOutput();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+				
+				break;
+				
+			case "move left":
+				try {
+                    this.currentPlayer.moveLeft();
+                    this.printOutput();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+				
+				break;
+				
+			case "move down":
+				try {
+                    this.currentPlayer.moveDown();
+                    this.printOutput();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+				
+				break;
+				
+			case "exit":
+				System.exit(0);
+				
+				break;
 
-	public static void main(String[] args) {
+			default:
+				break;
+			}
+		}
+		
+		input.close();
+	}
+
+	public static void main(String[] args) throws Exception {
 		Game game = new Game();
+		MapReader mapReader = new MapReader(game);
+		
+		mapReader.read();
 		
 		game.start();
 	}
