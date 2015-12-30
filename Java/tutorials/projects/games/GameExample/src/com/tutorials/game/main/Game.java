ff3977feae33f5acea822750080e7fa4072a7953
slavidlancer <slavidlancer@users.noreply.github.com>
2015-12-30 03:43:32 +0200

    Add the rest of the sources for the first game example in Java/tutorials

--- Java/tutorials/projects/games/GameExample/src/com/tutorials/game/main/Game.java
+++ Java/tutorials/projects/games/GameExample/src/com/tutorials/game/main/Game.java
@@ -2,9 +2,12 @@ package com.tutorials.game.main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import com.tutorials.game.display.Display;
+import com.tutorials.game.gfx.*;
+import com.tutorials.game.states.*;
 
 public class Game implements Runnable {
 	private Display display;
@@ -14,9 +17,19 @@ public class Game implements Runnable {
 	private boolean running = false;
 	private Thread thread;
 	
+	private InputHandler inputHandler;
 	private BufferStrategy bufferStrategy;
 	private Graphics graphics;
+	
 	private BufferedImage img;
+	private SpriteSheet spriteSheet;
+	
+	private State gameState;
+    private State menuState;
+    private State settingsState;
+	
+	public static Player player;
+	public static Rectangle enemy;
 	
 	public Game(String title, int width, int height) {
 		this.title = title;
@@ -26,6 +39,20 @@ public class Game implements Runnable {
 	
 	public void init() {
 		display = new Display(this.title, this.width, this.height);
+		img = ImageLoader.loadImage("/res/textures/bckg.jpg");
+		spriteSheet = new SpriteSheet(ImageLoader.loadImage(
+				"/res/textures/player.png"));
+		
+		this.inputHandler = new InputHandler(this.display);
+		Assets.init();
+		
+		gameState = new GameState();
+        menuState = new MenuState();
+        settingsState = new SettingsState();
+        StateManager.setCurrentState(gameState);
+		
+		player = new Player();
+		enemy = new Rectangle(50, 50, 20, 20);
 	}
 
 	@Override
@@ -79,15 +106,30 @@ public class Game implements Runnable {
 		graphics = bufferStrategy.getDrawGraphics();
 		graphics.clearRect(0, 0, this.width, this.height);
 		graphics.drawImage(img, 0, 0, this.width, this.height, null);
-		
+		player.render(graphics);
 		graphics.setColor(Color.RED);
+		graphics.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+		
+		if (StateManager.getCurrentState() != null){
+            StateManager.getCurrentState().render(this.graphics);
+        }
 		
 		bufferStrategy.show();
 		graphics.dispose();
 	}
 
 	private void tick() {
+		if (StateManager.getCurrentState() != null) {
+            StateManager.getCurrentState().tick();
+        }
 		
+        player.tick();
+        
+        if (player.intersects(enemy)) {
+            System.out.print("You were hit! Game over!");
+            
+            stop();
+        }
 	}
 
 	public synchronized void start() {
