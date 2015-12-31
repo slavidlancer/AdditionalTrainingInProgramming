1ffdabd08b8dae041ce6b7e2f20490c5c9c57985
slavidlancer <slavidlancer@users.noreply.github.com>
2015-12-31 02:23:53 +0200

    Update exam preparation_01 folders and sources in Java

--- Java/training_from_tutorials/exam_preparation/preparation_01/MagicCard/src/com/tutorials/exam/preparation/MagicCard.java
+++ Java/training_from_tutorials/exam_preparation/preparation_01/MagicCard/src/com/tutorials/exam/preparation/MagicCard.java
@@ -6,58 +6,58 @@ import java.util.Scanner;
 
 public class MagicCard {
 	private static boolean isSpecial(String s) {
-        return !(s.charAt(0) >= '1' && s.charAt(0) <= '9');
-    }
+		return !(s.charAt(0) >= '1' && s.charAt(0) <= '9');
+	}
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-        String[] cards = scan.nextLine().split("\\s+");
-        String oddOrEven = scan.nextLine();
-        String magicCard = scan.nextLine();
+		String[] cards = scan.nextLine().split("\\s+");
+		String oddOrEven = scan.nextLine();
+		String magicCard = scan.nextLine();
 
-        String magicCardValue = magicCard.replaceAll("[SHDC]", "");
-        String magicCardSuit = magicCard.replaceAll("[0-9JQKA]+", "");
+		String magicCardValue = magicCard.replaceAll("[SHDC]", "");
+		String magicCardSuit = magicCard.replaceAll("[0-9JQKA]+", "");
 
-        Map<Character, Integer> specialValues = new HashMap<>();
-        specialValues.put('J', 120);
-        specialValues.put('Q', 130);
-        specialValues.put('K', 140);
-        specialValues.put('A', 150);
+		Map<Character, Integer> specialValues = new HashMap<>();
+		specialValues.put('J', 120);
+		specialValues.put('Q', 130);
+		specialValues.put('K', 140);
+		specialValues.put('A', 150);
 
-        int magicSum = 0;
+		int magicSum = 0;
 
-        {
-            int i = 0;
+		{
+		    int i = 0;
 
-            if (oddOrEven.equals("odd")) {
-                i = 1;
-            }
+		    if (oddOrEven.equals("odd")) {
+		        i = 1;
+		    }
 
-            for (; i < cards.length; i += 2) {
+		    for (; i < cards.length; i += 2) {
 
-                String card = cards[i];
-                int cardValue;
+		        String card = cards[i];
+		        int cardValue;
 
-                if (isSpecial(card)) {
-                    cardValue = specialValues.get(card.charAt(0));
-                } else {
-                    cardValue = Integer.parseInt(card.replaceAll("[SHDC]", ""))
-                            * 10;
-                }
+		        if (isSpecial(card)) {
+		            cardValue = specialValues.get(card.charAt(0));
+		        } else {
+		            cardValue = Integer.parseInt(card.replaceAll(
+					"[SHDC]", "")) * 10;
+		        }
 
-                if (card.contains(magicCardValue)) {
-                    cardValue *= 3;
-                } else if (card.contains(magicCardSuit)) {
-                    cardValue *= 2;
-                }
+		        if (card.contains(magicCardValue)) {
+		            cardValue *= 3;
+		        } else if (card.contains(magicCardSuit)) {
+		            cardValue *= 2;
+		        }
 
-                magicSum += cardValue;
-            }
-        }
+		        magicSum += cardValue;
+		    }
+		}
 
-        System.out.println(magicSum);
-        
-        scan.close();
+		System.out.println(magicSum);
+		
+		scan.close();
 	}
 }
