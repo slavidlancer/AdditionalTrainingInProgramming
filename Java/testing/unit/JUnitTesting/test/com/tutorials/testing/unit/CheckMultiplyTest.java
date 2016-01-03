e271bd0fa9a2c11e400392c21c30aac224fc12e6
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-03 10:12:48 +0200

    Modify the JUnit project in order to run all tests, add failing tests

--- Java/testing/unit/JUnitTesting/test/com/tutorials/testing/unit/CheckMultiplyTest.java
+++ Java/testing/unit/JUnitTesting/test/com/tutorials/testing/unit/CheckMultiplyTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 
 public class CheckMultiplyTest {
     @Test
-    public void test() {
+    public void testCorrectDataWithIntegers() {
         MathClass tester = new MathClass();
         assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+    }
+    
+    @Test
+    public void testZeroValue() {
+        MathClass tester = new MathClass();
+        assertEquals("10 x 0 must be 0", 0, tester.multiply(10, 0));
     }
 }
