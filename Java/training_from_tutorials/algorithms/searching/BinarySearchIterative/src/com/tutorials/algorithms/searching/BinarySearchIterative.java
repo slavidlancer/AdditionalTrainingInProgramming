8bfbd1283d51afb6d0a6498e4cb60d17f9efb4a5
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-10 06:09:38 +0200

    Update BinarySearchIterative and BinarySearchRecursive in a new folder in Java searching algorithms

--- Java/training_from_tutorials/algorithms/searching/BinarySearchIterative/src/com/tutorials/algorithms/searching/BinarySearchIterative.java
+++
@@ -1,7 +0,0 @@
-package com.tutorials.algorithms.searching;
-
-public class BinarySearchIterative {
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-    }
-}
