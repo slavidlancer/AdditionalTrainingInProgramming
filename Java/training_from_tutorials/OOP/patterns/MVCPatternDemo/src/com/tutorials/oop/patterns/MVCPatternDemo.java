f8ab104afbb744e950d5ada480b5d1edc05e7a4b
slavidlancer <slavidlancer@users.noreply.github.com>
2016-01-10 05:42:13 +0200

    Update MVCPatternDemo in Java design patterns

--- Java/training_from_tutorials/OOP/patterns/MVCPatternDemo/src/com/tutorials/oop/patterns/MVCPatternDemo.java
+++ Java/training_from_tutorials/OOP/patterns/MVCPatternDemo/src/com/tutorials/oop/patterns/MVCPatternDemo.java
@@ -2,6 +2,21 @@ package com.tutorials.oop.patterns;
 
 public class MVCPatternDemo {
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        Student model = retriveStudentFromDatabase();
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(model, view);
+        
+        controller.updateView();
+        controller.setStudentName("new name");
+        System.out.println();
+        controller.updateView();
+    }
+    
+    private static Student retriveStudentFromDatabase() {
+        Student student = new Student();
+        student.setName("student name");
+        student.setRollNo("10");
+        
+        return student;
     }
 }
